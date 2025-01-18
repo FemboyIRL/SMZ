8d776533bb2e8ffd5e1ad6f03a8f5ec993f6b3d1
FemboyIRL <gatoelluis@gmail.com>
2025-01-17 19:52:17 -0600

    lol

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -65,8 +65,8 @@ exports.registerUser = async (params) => {
           });
         } else if (result.length === 0) {
           db.query(
-            `INSERT INTO users (fname, email, password, role) VALUES (?,?,?,?)`,
-            [fullName, email, hashedPassword, "1"],
+            `INSERT INTO users (fname, email, password) VALUES (?,?,?)`,
+            [fullName, email, hashedPassword],
             (err, result) => {
               if (err) {
                 reject({
